22b4d2d6c57c7e08b465d21c46adbb088f815f03
NicolasBroders <@NicolasBroders>
2018-11-04 20:59:13 +0100

    Gestion de projet avec Git et IntelliJ - Test 1

---
+++ src/main/java/deqo/nbro/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.nbro.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
