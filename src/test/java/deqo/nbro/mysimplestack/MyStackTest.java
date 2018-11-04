293cdc5133fffe323d244e780fb1d7de0d856bae
NicolasBroders <@NicolasBroders>
2018-11-04 21:58:09 +0100

    Gestion de projet avec Git et IntelliJ - Test 1

---
+++ src/test/java/deqo/nbro/mysimplestack/MyStackTest.java
@@ -0,0 +1,39 @@
+package deqo.nbro.mysimplestack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.*;
+
+public class MyStackTest {
+
+    private MyStack ms1,ms2;
+    private Item it;
+
+    @Before
+    public void setUp() {
+        ms1 = new MyStack();
+        ms2 = new MyStack();
+        it = new Item(2);
+        ms2.push(new Item(1));
+        ms2.push(it);
+    }
+
+    @Test (expected = EmptyStackException.class)
+    public void testStackEmpty() {
+        ms1.pop();
+    }
+
+    @Test
+    public void testGetSize() {
+        assertEquals(0,ms1.getSize());
+        assertEquals(2,ms2.getSize());
+    }
+
+    @Test
+    public void testNewStackSize() {
+        assertEquals("The size of a new stack should be zero",0,ms1.getSize());
+    }
+}
